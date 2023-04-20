d43f7b634d9253553adf2a8c67d21924085abf4b
nisha <nisha5778@gmail.com>
2023-04-19 23:29:41 -0500

    Basic structure ready

--- Hackathon/src/test/java/Recipes/Common_Code.java
+++ Hackathon/src/test/java/Recipes/Common_Code.java
@@ -11,7 +11,28 @@ import org.testng.annotations.Test;
 
 public class Common_Code {
 	public static WebDriver chromeDriver;
+	public static String strPCOSExcel = "C:/NumpyNinja_Projects/Hackathon/PCOS.xlsx";
+	public static String[] PCOSEliminate = {"Cake", "Pastries","White bread", "Fried", "Pizza", "Burger","Carbonated beverages",
+			 "sweet", "icecream","soda","juice","Red meat", "Processed meat", "Dairy",
+			 "Soy", "Gluten" ,"Pasta", "White rice", "Doughnut", "Fries", "Coffee", "Seed oils",
+			"vegetable oil", "soybean oil", "canola oil", "rapeseed oil", "sunflower oil", "safflower oil"};
 	
+	public static String[] HyperTensionEliminate = {"chips","pretzels","crackers", "coffee", "tea", "soft drink", 
+			"Alcohol", "Frozen food", "meat", "bacon", "ham", "Pickles", "Processed", "canned",
+			"Fried", "Sauce", "mayonnaise", "sausage","White rice","white bread", "pav"};
+
+	public static String[] strHeading = {"Recipe ID",
+			"Recipe Name: ",
+			"Recipe Category",
+			"Food Category", 
+			"Ingredients",
+			"Preparation Time",
+			"Cooking Time",
+			"Preparation Method",
+			"Nutrient Values",
+			"Targetted Morbid Conditions",
+			"Recipe URL"
+			};
 	@Test(priority = 1)
 	private void LoadPage() throws InterruptedException, IOException {
 		
